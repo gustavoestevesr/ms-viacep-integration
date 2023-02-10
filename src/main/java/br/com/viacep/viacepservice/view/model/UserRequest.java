1976bb72782c8d41be13def6f2fc674aefcbcc18
gustavoestevesr <gustavo.tech4me@gmail.com>
2023-02-10 11:35:38 -0300

    feat: Request e Response User construído

---
+++ src/main/java/br/com/viacep/viacepservice/view/model/UserRequest.java
@@ -0,0 +1,14 @@
+package br.com.viacep.viacepservice.view.model;
+
+public class UserRequest {
+    
+    private String cep;
+      
+    public String getCep() {
+        return cep;
+    }
+
+    public void setCep(String cep) {
+        this.cep = cep;
+    }
+}
---
+++ src/main/java/br/com/viacep/viacepservice/view/model/UserResponse.java
@@ -0,0 +1,67 @@
+package br.com.viacep.viacepservice.view.model;
+
+import java.math.BigDecimal;
+import java.util.HashMap;
+
+public class UserResponse {
+
+    private String cep;
+    private String logradouro;
+    private String bairro;
+    private String localidade;
+    private String uf;
+    private BigDecimal valorFrete;
+
+    HashMap<String, BigDecimal> fretePorCidade = new HashMap<String, BigDecimal>() {
+        {
+            put("Teresópolis", new BigDecimal("25.0"));
+            put("Nova Friburgo", new BigDecimal("15.0"));
+            put("Petrópolis", new BigDecimal("20.0"));
+            put("Rio de Janeior", new BigDecimal("30.0"));
+        }
+    };
+
+    public UserResponse(String cep, String logradouro, String bairro, String localidade, String uf,
+            BigDecimal valorFrete) {
+        this.cep = cep;
+        this.logradouro = logradouro;
+        this.bairro = bairro;
+        this.localidade = localidade;
+        this.uf = uf;
+        setValorFrete(valorFrete, localidade);
+    }
+
+    public BigDecimal getValorFrete() {
+        return valorFrete;
+    }
+
+    public void setValorFrete(BigDecimal valorFrete, String localidade) {
+        if (localidade == null) {
+            this.valorFrete = null;
+        } else if (fretePorCidade.containsKey(localidade)) {
+            this.valorFrete = fretePorCidade.get(localidade);
+        } else {
+            this.valorFrete = new BigDecimal("50.0");
+        }
+    }
+
+    public String getCep() {
+        return cep;
+    }
+
+    public String getLogradouro() {
+        return logradouro;
+    }
+
+    public String getBairro() {
+        return bairro;
+    }
+
+    public String getLocalidade() {
+        return localidade;
+    }
+
+    public String getUf() {
+        return uf;
+    }
+}
